77947f2f0063762ea556365641128db984075257
nplattii <neal.platt@gmail.com>
2015-10-06 10:22:17 -0500

    added comments

--- bin/buildBowtieIndecies.sh
+++ bin/buildBowtieIndecies.sh
@@ -1,7 +1,11 @@
-# Build bowtie indecies
-###############################
+# Build bowtie 2 indecies for Eptesicus and Myotis lucifugus genomes
+#  This is a requirement for miRDeep2
+
+#All work is done in the genome directory
 cd $GENOME_DIR
 bowtie-build $MLUC_GENOME $MLUC_GENOME &
 bowtie-build $EFUS_GENOME $EFUS_GENOME &
 
-#these are put in the background and need to be completed prior to running miRDeep2
+wait
+#these are put in the background and take a while.
+#  They need to be completed prior to miRDeep2
