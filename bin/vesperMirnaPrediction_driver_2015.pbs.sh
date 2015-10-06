146f6d9e89c8f68e8f7035b1ffed1c8d01ea7ab1
nealplatt <neal.platt@gmail.com>
2015-10-06 10:55:06 -0500

    converted to pbs script (78 processors) and added comments

--- bin/vesperMirnaPrediction_driver_2015.pbs.sh
+++ bin/vesperMirnaPrediction_driver_2015.pbs.sh
@@ -1,17 +1,47 @@
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N myoSmallRNAs
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q Chewie,R2D2
+#$ -pe fill 78
+#$ -P communitycluster
+
+cd /lustre/scratch/roplatt/myotisSmallRNAs
+
 #set variables that are used throughout the analysis
 bin/setVariables.sh
 
 #create the major subdirectories
 #bin/mkDirStructure.sh
 
+#-------------------------
+# Get seq data and QC it.
+
+#-------------------------
+# Get genomes and clean up
+
+
 #get bat miRNAs from miRBase into fasta form
-bin/getMiRNAs.sh
+#bin/getMiRNAs.sh
 
 #start indexing the genomes (takes some time)
-bin/buildBowtieIndecies.sh
+#bin/buildBowtieIndecies.sh
 
 #start the miRDeep2 prediction process
 bin/predictWmiRDeep2.sh
 
 #mask each of the genomes for "Chiropteran" repeats
-bin/runRepeatMasker.sh
+#  This is necessary to create a bed file for post processing
+#  the miRNAs (seeing wich overlap with TEs).
+# bin/runRepeatMasker.sh
+
+#wait for all jobs to finish before exiting.
+wait
+
+echo "$JOB_NAME finished" | mailx -s "$JOB_NAME finished" 9034521885@txt.att.net
+
+sleep 10
+https://github.com/nealplatt/vesperMiRNAs.git
