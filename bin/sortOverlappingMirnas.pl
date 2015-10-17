4e3f12fb350c1c5937de2765ab9f626653117842
nealplatt <neal.platt@gmail.com>
2015-10-16 20:15:48 -0500

    lots of errors to be fixed - print overlapping

--- bin/sortOverlappingMirnas.pl
+++ bin/sortOverlappingMirnas.pl
@@ -50,7 +50,7 @@ while(<BED_INPUT_FILE>){
 }	
 
 #temporary test to see if data is stored properly
-#print Dumper \%duplicates;	
+print Dumper \%multiples;	
 
 #for each of the scaffolds with multiple miRNAs - see if the miRNAs records overlap e/o.
 foreach my $dupContigs (sort keys %multiples){
@@ -67,7 +67,7 @@ foreach my $dupContigs (sort keys %multiples){
 		$color=$multiples{$dupContigs}{$miRNA_name}{color};
 			
 		#put all of the metrics into an array of arrays to be sorted
-		push @contigHits, [$start, $end, $score, $orient, $color];
+		push @contigHits, [$start, $end, $score, $orient, $color, $miRNA_name];
 
 	}
 
@@ -81,17 +81,73 @@ foreach my $dupContigs (sort keys %multiples){
 	while( $i <= $#sortedContigHits ){
 
 
+		#store array values in simple variables
+		$start=$sortedContigHits[$i][0];
+		$end=$sortedContigHits[$i][1];
+		$score=$sortedContigHits[$i][2];
+		$orient=$sortedContigHits[$i][3];
+		$color=$sortedContigHits[$i][4];
+		$name=$sortedContigHits[$i][5];
+
+		#$next_start=$sortedContigHits[$i+1][0];
+		#$next_end=$sortedContigHits[$i+1][1];
+		#$next_score=$sortedContigHits[$i+1][2];
+		#$next_orient=$sortedContigHits[$i+1][3];
+		#$next_color=$sortedContigHits[$i+1][4];
+		#$next_name=$sortedContigHits[$i+1][5];
+
+		#find if overlapping
+		if($i+1 > $#sortedContigHits){
+			$next_start=undef;
+			$next_end=undef;
+			$next_score=undef;
+			$next_orient=undef;
+			$next_color=undef;
+			$next_name=undef;
+		}else{
+			$next_start=$sortedContigHits[$i+1][0];
+			$next_end=$sortedContigHits[$i+1][1];
+			$next_score=$sortedContigHits[$i+1][2];
+			$next_orient=$sortedContigHits[$i+1][3];
+			$next_color=$sortedContigHits[$i+1][4];
+			$next_name=$sortedContigHits[$i+1][5];
+		}
 
 
-		print "$dupContigs\t$sortedContigHits[$i][0]\t$sortedContigHits[$i][1]\t$miRNA_name\t$sortedContigHits[$i][2]\t$sortedContigHits[$i][3]$sortedContigHits[$i][0]\t$sortedContigHits[$i][1]\t$sortedContigHits[$i][4]\n";		
+		if( ($start < $next_start && $end > $next_start)){
+			#print "overlap\t$dupContigs\t$start\t$end\t$name\t$score\t$orient\t$start\t$end\t$color\n";
+		}
+		print "$dupContigs\t$start\t$end\t$name\t$score\t$orient\t$start\t$end\t$color\n";
+		
 		$i++;
+
+		
+		my $start;
+		my $end;
+		my $score;
+		my $orient;
+		my $color;
+		my $name;
+		my $next_start;
+		my $next_end;
+		my $next_score;
+		my $next_orient;
+		my $next_color;
+		my $next_name;
+
+	
+
 	}
-		#!!!!!!!!!!!!!!!this has not been error checked !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!				
+
 
 
 		
 }
 
+
+# ||
+#		    ($start    < $next_start && $start > $next_end) ||
+#		    ($next_end > $start      && $next_end < $start)   
 
 
 
